a0e389793791b564b56bc4ae0085010a1e5fbbad
Hexe-N <61705339+Hexe-N@users.noreply.github.com>
2022-04-11 15:28:38 +0300

    Add files via upload

---
+++ All/Program/Program/Figures/PolyLine.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace VectorEditor.Figures
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Класс полилинии 
+    /// </summary>
+    [JsonObject(MemberSerialization.Fields)]
+    public class Polyline: BaseFigure
+    {
+        /// <summary>
+        /// Конструктор класса полилинии 
+        /// </summary>
+        public Polyline()
+        {
+            _lineProperties = new LineProperties();
+            _points = new Points();
+        }        
+
+    }
+}
